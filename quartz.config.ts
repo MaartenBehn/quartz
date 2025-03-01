63277d147f2ac2c33da735cd9a1bd52e0cfda224
Maarten Behn <maarten.behn@gmail.com>
2025-03-01 17:36:17 +0100

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Uni Notes",
-    pageTitleSuffix: "von Maarten Behn",
+    pageTitle: "🪴 Maarten's Uni Notes",
+    pageTitleSuffix: " (Maarten's Uni Notes)",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
